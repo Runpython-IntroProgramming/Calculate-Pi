fdd14164d05004908eda87191fd5876e9f66b927
Eric Dennison <ericd@netdenizen.com>
2015-09-10 21:19:08 -0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+# create the tests folder
+mkdir tests
+cd tests
+git init
+git pull https://${GH_REPO}
+cd ..
+
+python tests/test.py ${TESTMODULE}
