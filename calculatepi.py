79876d0d35933e8afa2d238a56645251d251dac7
Eric Dennison <ericd@netdenizen.com>
2015-09-10 21:01:57 -0400

    Update calculatepi.py

--- calculatepi.py
+++ calculatepi.py
@@ -4,6 +4,17 @@ Author: <your name here>
 Credit: <list sources used, if any>
 Assignment:
 
-For details, see: https://github.com/HHS-IntroProgramming/Calculate-Pi/blob/master/README.md
+Write and submit a Python program that computes an approximate value of π by calculating the following sum:
+
+(see: https://github.com/HHS-IntroProgramming/Calculate-Pi/blob/master/README.md)
+
+This sum approaches the true value of π as n approaches ∞.
+
+Your program must ask the user how many terms to use in the estimate of π, the number of significant
+figures to show, then print the estimate using the correct number of significant figures. For example:
+
+I will estimate pi. How many terms should I use? 100
+How many sig figs should I use in the result? 8
+The approximate value of pi is 3.1315929
 
 """
